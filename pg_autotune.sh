2a4b3cca31ddf5c7dc46f8d059873715a03eea5a
Alexander Sosna <alexander.sosna@credativ.de>
2016-04-13 13:31:04 +0200

    Add: Calculating more parameters

--- pg_autotune.sh
+++ pg_autotune.sh
@@ -24,16 +24,11 @@
 set -e
 
 # Settings
-grubcfg="grub"
-pgconf="./postgresql.conf"
+grubcfg="testdata/grub"
+pgconf="testdata/postgresql.conf"
 pgversion="9.5"
 pgclustername="main"
 
-
-# Autoconf
-cpucount=$(nproc)
-totalram_kB=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
-totalram_MB=$(($totalram_kB/1000))
 
 # set_option_in_file changes lines in a file in order to change configuration options.
 # It takes 3 arguments:
@@ -66,7 +61,7 @@ function configure_grub()
 # Change PostgreSQL configuration
 function configure_postgresql()
 {
-    if command -v pg_conftool2 >/dev/null 2>&1; then
+    if command -v pg_conftool >/dev/null 2>&1; then
         pg_conftool "$pgconf" set "$1" "$2"
     else
         set_option_in_file "$pgconf" "\b${1} =" "$1 = $2"
@@ -79,6 +74,11 @@ configure_grub "elevator=" 'GRUB_CMDLINE_LINUX="elevator=deadline"'
 # echo "Update grub configuration"
 #update-grub2
 
+# Autoconf
+cpucount=$(nproc)
+totalram_kB=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
+totalram_MB=$(($totalram_kB/1000))
+
 echo "Generate PostgreSQL parameters"
 shared_buffers_MB=$(($totalram_MB / 4))
 if (( $shared_buffers_MB > 12000 ))
@@ -86,6 +86,10 @@ then
     shared_buffers_MB=12000
 fi
 
+work_mem_MB=16
+maintenance_work_mem_MB=512
+autovacuum_max_workers=$(( $cpucount + 2 ))
+effective_cache_size_MB=$(( $totalram_MB / 2 ))
 
 echo "Tune postgresql.conf"
 # CONNECTIONS AND AUTHENTICATION
@@ -95,23 +99,23 @@ configure_postgresql "superuser_reserved_connections" "5"
 
 # RESOURCE USAGE (except WAL)
 configure_postgresql "shared_buffers" "${shared_buffers_MB}MB"
-configure_postgresql "work_mem" "16MB"
-configure_postgresql "maintenance_work_mem" "512MB"
-configure_postgresql "shared_preload_libraries" "'pg_stat_statements'"
+configure_postgresql "work_mem" "${work_mem_MB}"
+configure_postgresql "maintenance_work_mem" "${maintenance_work_mem_MB}MB"
+configure_postgresql "shared_preload_libraries" "pg_stat_statements"
 
 # WRITE AHEAD LOG
 configure_postgresql "wal_level" "minimal"
 configure_postgresql "wal_buffers" "16MB"
 configure_postgresql "wal_compression" "off"
 configure_postgresql "checkpoint_timeout" "5min"
-configure_postgresql "max_wal_size" "2GB"
+configure_postgresql "max_wal_size" "4GB"
 configure_postgresql "min_wal_size" "80MB"
 configure_postgresql "checkpoint_completion_target" "0.7"
 
 # AUTOVACUUM PARAMETERS
 configure_postgresql "autovacuum" "on"
-configure_postgresql "autovacuum_max_workers" "4"
+configure_postgresql "autovacuum_max_workers" $autovacuum_max_workers
 
 # QUERY TUNING
 configure_postgresql "random_page_cost" "4.0"
-configure_postgresql "effective_cache_size" "4GB"
+configure_postgresql "effective_cache_size" "${effective_cache_size_MB}MB"
