9a276cf47e917185d03b51eee140a0e59dd126d4
Alexander Sosna <alexander.sosna@credativ.de>
2017-09-18 15:44:17 +0200

    Add shared_buffers()

--- pg_cloudconfig.py
+++ pg_cloudconfig.py
@@ -19,6 +19,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import argparse
 import psutil
 import logging
+import math
+
+# Global variables
+SUPPORTED_VERSIONS = ['9.6']
 
 def memory():
     """
@@ -37,47 +41,103 @@ def memory():
         mem['used'] = int(mem['total']) - int(mem['free'])
     return mem
 
+def round_power_of_2_ceil(number):
+    return int(math.pow(2,math.ceil(math.log(number,2))))
+
+def round_power_of_2_floor(number):
+    return int(math.pow(2,math.floor(math.log(number,2))))
+
+def shared_buffers(pg_in, system, log):
+    # Get total available memory as MB
+    total = system['memory']['total'] / 1024
+
+    # Get a candidate value
+    candidate = total / 4
+
+    # Special cases for small memory
+    if total < 4096:
+        sb = 256
+        if total < 1024:
+            sb = 128
+        if total < 512:
+            sb = 64
+        if total < 256:
+            sb = 16
+        if total < 128:
+            sb = 4
+    elif candidate > 16384:
+        sb = 16384
+    else:
+        sb = candidate
+
+    sb = round_power_of_2_floor(sb)
+    return str(sb)+"MB"
+
+def tune(pg_in, system, log):
+    """
+    Set multiple PostgreSQL settings according to the given input
+    """
+    if not pg_in['version'] in SUPPORTED_VERSIONS:
+        log.error("Version is not supported, "+ pg_in['version'])
+
+    pg_out = {}
+    pg_out['shared_buffers'] = shared_buffers(pg_in, system, log)
+
+
+    return pg_out
+
 def main():
     # Configure logging
     log = logging.getLogger('pg_cloudconfig')
     log.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -
+    # %(message)s')
     formatter = logging.Formatter('%(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     log.addHandler(ch)
 
     # Get system information
-    cpu_count = psutil.cpu_count()
-    mem = memory()
+    system = {}
+    system['cpu_count'] = psutil.cpu_count()
+    system['memory'] = memory()
 
     # Get cmd arguments
-    parser = argparse.ArgumentParser(description='Tool to set optimized defaults for PostgreSQL in cloud environments.')
-    parser.add_argument('--pg_version', default="9.6", help='Version of the PostgreSQL cluster to tune.')
-    parser.add_argument('--pg_clustername', default="main", help='Name of the PostgreSQL cluster to tune.')
-    parser.add_argument('--pg_conf_dir', default="", help='Path to the dir holding the postgresql.conf (normally not necessary to set explicitly!).')
+    parser = argparse.ArgumentParser(
+        description='Tool to set optimized defaults for PostgreSQL in cloud environments.')
+    parser.add_argument('--pg_version', default="9.6",
+                        help='Version of the PostgreSQL cluster to tune.')
+    parser.add_argument('--pg_clustername', default="main",
+                        help='Name of the PostgreSQL cluster to tune.')
+    parser.add_argument('--pg_conf_dir', default="",
+                        help='Path to the dir holding the postgresql.conf (normally not necessary to set explicitly!).')
     args = parser.parse_args()
 
     # Settings - set defaults
-    pg_version = args.pg_version
-    pg_clustername = args.pg_clustername
-    pg_conf_dir="/etc/postgresql/"+pg_version+"/"+pg_clustername
+    pg ={}
+    pg['version'] = args.pg_version
+    pg['clustername'] = args.pg_clustername
+    pg['conf_dir'] = "/etc/postgresql/" + pg['version'] + "/" + pg['clustername']
 
     # If --pg_conf_dir is set, use it insted of default
     if args.pg_conf_dir > "":
         pg_conf_dir = args.pg_conf_dir
-    pg_conf=pg_conf_dir+"/postgresql.conf"
+    pg['conf'] = pg['conf_dir'] + "/postgresql.conf"
 
-    # Debug information
+    # Show information
     log.info("Variables")
-    log.info("pg_version: "+pg_version)
-    log.info("pg_clustername: "+pg_clustername)
-    log.info("pg_conf_dir: "+pg_conf_dir)
-    log.info("pg_conf: "+pg_conf)
-    log.info("cpu_count: "+str(cpu_count))
-    log.info("mem: "+str(mem))
+    log.info("pg_version: " + pg['version'])
+    log.info("pg_clustername: " + pg['clustername'])
+    log.info("pg_conf_dir: " + pg['conf_dir'])
+    log.info("pg_conf: " + pg['conf'])
+    log.info("cpu_count: " + str(system['cpu_count']))
+    log.info("mem: " + str(system['memory']))
 
+    pg_out = tune(pg,system,log)
+
+    log.info("Result")
+    log.info(pg_out)
 
 if __name__ == "__main__":
     main()
