0b90bdd1afa48c09ddc6bf6746a289ec26a85d2c
Alexander Sosna <alexander.sosna@credativ.de>
2017-09-19 14:23:23 +0200

    Minimal functional version

--- pg_cloudconfig.py
+++ pg_cloudconfig.py
@@ -21,12 +21,13 @@ import psutil
 import logging
 import math
 from pint import UnitRegistry
-
+from subprocess import call
 
 # Global variables
 SUPPORTED_VERSIONS = ['9.6']
 ureg = UnitRegistry()
 Q_ = ureg.Quantity
+
 
 def memory():
     """
@@ -45,20 +46,24 @@ def memory():
         mem['used'] = mem['total'] - mem['free']
     return mem
 
+
 def round_power_of_2_ceil(number):
-    return int(math.pow(2,math.ceil(math.log(number,2))))
+    return int(math.pow(2, math.ceil(math.log(number, 2))))
+
 
 def round_power_of_2_ceil_mb(mem):
     mem_mb = mem.to(ureg.megabyte).magnitude
-    ceil_mb = int(math.pow(2,math.ceil(math.log(mem_mb,2))))
+    ceil_mb = int(math.pow(2, math.ceil(math.log(mem_mb, 2))))
     return ceil_mb * ureg.megabyte
 
+
 def round_power_of_2_floor(number):
-    return int(math.pow(2,math.floor(math.log(number,2))))
+    return int(math.pow(2, math.floor(math.log(number, 2))))
+
 
 def round_power_of_2_floor_mb(mem):
     mem_mb = mem.to(ureg.megabyte).magnitude
-    floor_mb = int(math.pow(2,math.floor(math.log(mem_mb,2))))
+    floor_mb = int(math.pow(2, math.floor(math.log(mem_mb, 2))))
     return floor_mb * ureg.megabyte
 
 
@@ -87,6 +92,7 @@ def shared_buffers(pg_in, system, log):
 
     return round_power_of_2_floor_mb(sb)
 
+
 def maintenance_work_mem(pg_in, system, log):
     # Get total available memory as MB
     total = system['memory']['total']
@@ -101,24 +107,29 @@ def maintenance_work_mem(pg_in, system, log):
         mwm = candidate
     return round_power_of_2_floor_mb(mwm)
 
+
 def max_connections(pg_out, pg_in, system, log):
     # If max_connections are given we use it
     if pg_in['max_connections'] > 0:
         return pg_in['max_connections']
 
     # Possible max_connections based on cpu_count
-    cpu_candidate = system['cpu_count'] * 15
+    cpu_candidate = system['cpu_count'] * 12
 
     connection_ram = system['memory']['total'].to(ureg.megabyte).magnitude
+    # keep some space for the OS fs cache
+    connection_ram -= system['memory'][
+        'total'].to(ureg.megabyte).magnitude / 10
     connection_ram -= pg_out['shared_buffers'].to(ureg.megabyte).magnitude
-    connection_ram -= pg_out['shared_buffers'].to(ureg.megabyte).magnitude # keep some space for the fs cache
-    connection_ram -= pg_out['maintenance_work_mem'].to(ureg.megabyte).magnitude
+    connection_ram -= pg_out[
+        'maintenance_work_mem'].to(ureg.megabyte).magnitude
 
     # Estimate possible workmem
     workmem_candidate = round_power_of_2_floor(connection_ram / cpu_candidate)
 
     # Calculate possible max_connections based on workmem only
-    return int(round(connection_ram / workmem_candidate,-1))
+    return int(round(connection_ram / workmem_candidate, -1))
+
 
 def work_mem(pg_out, pg_in, system, log):
     connection_ram = system['memory']['total']
@@ -126,21 +137,61 @@ def work_mem(pg_out, pg_in, system, log):
     connection_ram -= pg_out['maintenance_work_mem']
     return round_power_of_2_floor_mb(connection_ram / pg_out['max_connections'])
 
+
+def effective_cache_size(pg_out, pg_in, system, log):
+    usage_factor = 0.6
+    connection_ram = system['memory']['total']
+    connection_ram -= pg_out['shared_buffers']
+    connection_ram -= pg_out['maintenance_work_mem']
+    connection_ram -= pg_out['work_mem'] * pg_out[
+        'max_connections'] * usage_factor
+    return round_power_of_2_floor_mb(connection_ram)
+
+
+def superuser_reserved_connections(pg_out, pg_in, system, log):
+    src = 7
+    if pg_out['max_connections'] <= (src * 30):
+        src = 5
+    elif pg_out['max_connections'] >= (src * 100):
+        src = 10
+    return src
+
+
 def tune(pg_in, system, log):
     """
     Set multiple PostgreSQL settings according to the given input
     """
     if not pg_in['version'] in SUPPORTED_VERSIONS:
-        log.error("Version is not supported, "+ pg_in['version'])
+        log.error("Version is not supported, " + pg_in['version'])
 
     pg_out = {}
     pg_out['shared_buffers'] = shared_buffers(pg_in, system, log)
     pg_out['maintenance_work_mem'] = maintenance_work_mem(pg_in, system, log)
     pg_out['max_connections'] = max_connections(pg_out, pg_in, system, log)
     pg_out['work_mem'] = work_mem(pg_out, pg_in, system, log)
-
-
+    pg_out['effective_cache_size'] = effective_cache_size(
+        pg_out, pg_in, system, log)
+    pg_out['superuser_reserved_connections'] = superuser_reserved_connections(
+        pg_out, pg_in, system, log)
     return pg_out
+
+def format_for_pg_conf(si):
+    if isinstance(si, int) or isinstance(si, str):
+        # Seams to be int or string so we just return the value
+        return str(si)
+
+    base = si.to_base_units()
+    if base.units == "bit":
+        si_mb = si.to(ureg.megabyte).magnitude
+        return str(si_mb) + "MB"
+
+def persist_conf(pg_out, pg_in, log):
+    for key, value in pg_out.items():
+        setting = format_for_pg_conf(value)
+        print(key+": "+str(setting))
+        ret= call(["pg_conftool", pg_in['version'], pg_in['clustername'], "set", key, setting])
+        if ret != 0:
+            log.error("Error while setting: "+key+" to: "+setting+" with return code: "+ret)
 
 def main():
     # Configure logging
@@ -173,10 +224,11 @@ def main():
     args = parser.parse_args()
 
     # Settings - set defaults
-    pg ={}
+    pg = {}
     pg['version'] = args.pg_version
     pg['clustername'] = args.pg_clustername
-    pg['conf_dir'] = "/etc/postgresql/" + pg['version'] + "/" + pg['clustername']
+    pg['conf_dir'] = "/etc/postgresql/" + \
+        pg['version'] + "/" + pg['clustername']
 
     # If --pg_conf_dir is set, use it insted of default
     if args.pg_conf_dir > "":
@@ -197,10 +249,11 @@ def main():
     log.info("cpu_count: " + str(system['cpu_count']))
     log.info("mem: " + str(system['memory']))
 
-    pg_out = tune(pg,system,log)
+    pg_out = tune(pg, system, log)
 
     log.info("Result")
     log.info(pg_out)
+    persist_conf(pg_out,pg, log)
 
 if __name__ == "__main__":
     main()
