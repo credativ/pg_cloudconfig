5b86e37857380033a2d9b6c8310a1eb60fd1794c
Alexander Sosna <alexander.sosna@credativ.de>
2017-12-22 16:06:03 +0100

    Make it more simple, remove redundant version

--- pg_cloudconfig/pg_cloudconfig.py
+++ pg_cloudconfig/pg_cloudconfig.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import argparse
 import os
 import logging
 import math
@@ -27,7 +28,8 @@ from datetime import datetime
 from statistics import median
 from pint import UnitRegistry
 # Global variables
-VERSION = 0.4
+__version__ = '0.5'
+VERSION = __version__
 SUPPORTED_VERSIONS = ['10', '9.6']
 LOG_LEVEL = logging.INFO
 
@@ -148,8 +150,8 @@ def work_mem(pg_out, system):
     connection_ram = system['memory']['total']
     connection_ram -= pg_out['shared_buffers']
     connection_ram -= pg_out['maintenance_work_mem']
-    return round_power_of_2_floor_mb(connection_ram /
-                                     pg_out['max_connections'])
+    return round_power_of_2_floor_mb(
+        connection_ram / pg_out['max_connections'])
 
 
 def effective_cache_size(pg_out, pg_in, system):
@@ -239,8 +241,8 @@ def persist_conf(pg_out, pg_in, log):
 
 def repeat_to_length(string_to_expand, length):
     """Repeat a string (partial) to a given length"""
-    return (string_to_expand *
-            int((length / len(string_to_expand)) + 1))[:length]
+    return (
+        string_to_expand * int((length / len(string_to_expand)) + 1))[:length]
 
 
 def chomp(x):
@@ -342,10 +344,137 @@ def tune(pg_in, system, no_static, log):
     pg_out['maintenance_work_mem'] = maintenance_work_mem(system)
     pg_out['max_connections'] = max_connections(pg_out, pg_in, system)
     pg_out['work_mem'] = work_mem(pg_out, system)
-    pg_out['effective_cache_size'] = effective_cache_size(pg_out, pg_in,
-                                                          system)
+    pg_out['effective_cache_size'] = effective_cache_size(
+        pg_out, pg_in, system)
     pg_out['superuser_reserved_connections'] = superuser_reserved_connections(
         pg_out)
     pg_out['autovacuum_max_workers'] = autovacuum_max_workers(system)
     pg_out['vacuum_cost_limit'] = vacuum_cost_limit(pg_in)
     return pg_out
+
+
+def main():
+    """Main function ;)"""
+    system = {}
+    system['cpu_count'] = os.cpu_count()
+    system['memory'] = memory()
+
+    # Get cmd arguments
+    parser = argparse.ArgumentParser(
+        description=
+        """Tool to set optimized defaults for PostgreSQL in virtual environments
+        (changes settings without asking for confirmation).""",
+        epilog="""Should be run as the same user as PostgreSQL.
+        pg_version and pg_clustername are used to choose a cluster.
+         It is assumed that the Debian / postgresql-common naming and
+         configuration schema is used.
+         If this is not the case --pg_conf_dir needs to be set.
+         pg_conftool is used to get/set settings and is required!
+         This does not tune PostgreSQL for any specific workload but only
+         tries to set some optimized defaults based on a few input variables
+         and simple rules.""")
+    parser.add_argument(
+        'pg_version',
+        nargs=1,
+        help='version of the PostgreSQL cluster to tune')
+    parser.add_argument(
+        'pg_clustername',
+        nargs=1,
+        help='name of the PostgreSQL cluster to tune')
+    parser.add_argument(
+        '--max_connections',
+        default="",
+        help='set the max_connections explicitly if needed')
+    parser.add_argument(
+        '--pg_conf_dir',
+        default="",
+        help='path to dir holding the postgresql.conf (to override default)')
+    parser.add_argument(
+        '--dynamic_only',
+        action='store_true',
+        help='do not set static optimized defaults')
+    parser.add_argument(
+        '--blacklist',
+        nargs='+',
+        default=[],
+        metavar='SETTING',
+        help='settings not to touch')
+    parser.add_argument(
+        '--debug', action='store_true', help='show debug messages')
+    parser.add_argument(
+        '-q', '--quiet', action='store_true', help='disable output')
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s {version}'.format(version=VERSION))
+    args = parser.parse_args()
+
+    # Configure logging
+    if args.debug:
+        log_level = logging.DEBUG
+    elif args.quiet:
+        log_level = logging.FATAL
+    else:
+        log_level = LOG_LEVEL
+
+    log = logging.getLogger('pg_cloudconfig')
+    log.setLevel(log_level)
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -
+    # %(message)s')
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+    # Settings
+    pg = {}
+    pg['version'] = args.pg_version[0]
+    pg['clustername'] = args.pg_clustername[0]
+    pg['blacklist'] = args.blacklist
+
+    # If --pg_conf_dir is set, use it insted of default
+    if args.pg_conf_dir > "":
+        pg['conf_dir'] = os.path.join(args.pg_conf_dir, pg['conf'],
+                                      pg['conf_dir'], "postgresql.conf")
+    else:
+        pg['conf_dir'] = os.path.join("/etc/postgresql", pg['version'],
+                                      pg['clustername'])
+    pg['conf'] = os.path.join(pg['conf_dir'], "postgresql.conf")
+
+    if args.max_connections > "":
+        pg['max_connections'] = args.max_connections
+    else:
+        pg['max_connections'] = -1
+
+    log.info("Cluster to tune:\t %s/%s", pg['version'], pg['clustername'])
+    log.info("conf_dir:\t %s", pg['conf_dir'])
+
+    if not os.path.isdir(pg['conf_dir']):
+        log.error("conf_dir (%s) is not a directory or does not exist",
+                  pg['conf_dir'])
+        log.info(
+            "Hint: Does the cluster %s/%s exists? Try 'pg_createcluster %s %s' if not.",
+            pg['version'], pg['clustername'], pg['version'], pg['clustername'])
+        sys.exit(1)
+
+    pg['data_directory'] = data_directory(pg)
+    log.info("data_directory:\t %s", pg['data_directory'])
+
+    log.info("Start write_bench...")
+    pg['disk_speed'] = write_bench(
+        os.path.join(pg['data_directory'], "~write_test.dat"), log)
+    log.info("Disk was benched as: %s (slow|medium|fast)", pg['disk_speed'])
+
+    log.info("Calculate settings...")
+    no_static = args.dynamic_only
+    pg_out = tune(pg, system, no_static, log)
+    log.debug("Result")
+    log.debug(pg_out)
+
+    log.info("Persist settings using pg_conftool...")
+    persist_conf(pg_out, pg, log)
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
+import codecs
+import os
+import re
 from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='pg_cloudconfig',
-    version='0.4',
+    version=find_version("pg_cloudconfig", "pg_cloudconfig.py"),
     license='GPL3',
     author='Alexander Sosna',
     author_email='alexander.sosna@credativ.de',
@@ -28,7 +48,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'pg_cloudconfig = pg_cloudconfig.command:main',
+            'pg_cloudconfig = pg_cloudconfig.pg_cloudconfig:main',
         ],
     },
 )
