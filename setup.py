6ee6bf00d50fcfa49fc7c503870bd288754ce04c
Alexander Sosna <alexander.sosna@credativ.de>
2017-12-22 11:45:05 +0100

    Format lines for flake8

--- setup.py
+++ setup.py
@@ -7,17 +7,21 @@ setup(
     license='GPL3',
     author='Alexander Sosna',
     author_email='alexander.sosna@credativ.de',
-    description='Tool to set optimized defaults for PostgreSQL in virtual environments.',
-    keywords = "postgres postgresql tune tuning cloud",
-    url = "https://github.com/credativ/pg_cloudconfig",
-    packages=find_packages(exclude=('tests','doc',)),
+    description=
+    'Tool to set optimized defaults for PostgreSQL in virtual environments.',
+    keywords="postgres postgresql tune tuning cloud",
+    url="https://github.com/credativ/pg_cloudconfig",
+    packages=find_packages(exclude=(
+        'tests',
+        'doc',
+    )),
     zip_safe=True,
     platforms='any',
     classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: System Administrators',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3'
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
     ],
     install_requires=[
         'pint',
